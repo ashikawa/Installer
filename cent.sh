3a1b493d724b8db54e322eb54b2d9a2676e405d5
a.shigeru <a.shigeru@gmail.com>
2012-10-04 16:55:24 +0900

    SE Linux の解除用設定

--- cent.sh
+++ cent.sh
@@ -5,3 +5,7 @@ yum -y update
 
 echo "yum install -y gcc gcc-c++ make openssl-devel git subversion"
 yum install -y gcc gcc-c++ make openssl-devel git subversion
+
+# SE Linux のオフ
+setenforce 0
+sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
