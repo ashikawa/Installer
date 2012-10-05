415f39ffbf56191c31777b0cbc98b726c6b0e922
a.shigeru <a.shigeru@gmail.com>
2012-10-05 11:20:57 +0900

    php-apc の追加

--- php.sh
+++ php.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "yum install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql"
-yum install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql
+yum install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql php-pecl-apc
 
 echo "/etc/init.d/httpd restart"
 /etc/init.d/httpd restart
