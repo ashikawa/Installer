58df2eee5347b3abe3f00f51b9089c4e0f5ca0b0
a.shigeru <a.shigeru@gmail.com>
2013-01-24 17:21:57 +0900

    php.ini memory_limit

--- php.sh
+++ php.sh
@@ -3,5 +3,7 @@
 echo "install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql php-pecl-apc"
 yum install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql php-pecl-apc
 
+sed -i -e 's#memory_limit = 256M#memory_limit = 512M#g' /etc/php.ini
+
 echo "/etc/init.d/httpd restart"
 /etc/init.d/httpd restart
