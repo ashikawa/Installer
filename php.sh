14c20e29ee98c56a0a5d13b8d899ef0910c16d70
a.shigeru <a.shigeru@gmail.com>
2013-02-05 17:33:56 +0900

    php.ini date.timezone

--- php.sh
+++ php.sh
@@ -4,6 +4,7 @@ echo "install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql
 yum install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql php-pecl-apc
 
 sed -i -e 's#memory_limit = .*M#memory_limit = 512M#g' /etc/php.ini
+sed -i -e 's#;date.timezone =#date.timezone = "Asia/Tokyo"#g' /etc/php.ini
 
 echo "/etc/init.d/httpd restart"
 /etc/init.d/httpd restart
