0cdb9c19c8c16d4f1619d3be8e81e6f927cd0d48
m-s-modified <a.shigeru@gmail.com>
2013-03-18 12:45:49 +0900

    Update php.sh
    GDライブラリを標準装備に

--- php.sh
+++ php.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "install -y php php-devel php-mbstring php-pear php-xml php-mysql php-pgsql php-pecl-apc"
-yum install -y php php-devel php-cli php-mbstring php-pear php-xml php-mysql php-pgsql php-pecl-apc
+echo "install -y php php-devel php-cli php-mbstring php-pear php-xml php-gd php-mysql php-pgsql php-pecl-apc"
+yum install -y php php-devel php-cli php-mbstring php-pear php-xml php-gd php-mysql php-pgsql php-pecl-apc
 
 sed -i -e 's#memory_limit = .*M#memory_limit = 512M#g' /etc/php.ini
 sed -i -e 's#;date.timezone =#date.timezone = "Asia/Tokyo"#g' /etc/php.ini
