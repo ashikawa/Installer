b9a6ea8c1aac3eef3531d6f5c872a3235e118eb8
m-s-modified <a.shigeru@gmail.com>
2013-05-30 16:14:26 +0900

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -2,37 +2,6 @@
 
 # WordPress
 
-echo "Enter database name"
-read DATABASE_NAME
-while [ "$DATABASE_NAME" == "" ]
-do
-    read DATABASE_NAME
-done
-
-echo "Enter mysql user name:"
-read MYSQL_USER
-while [ "$MYSQL_USER" == "" ]
-do
-    read MYSQL_USER
-done
-
-echo "Enter $MYSQL_USER's password:"
-read MYSQL_PASSWORD
-while [ "$MYSQL_PASSWORD" == "" ]
-do
-    read MYSQL_PASSWORD
-done
-
-
-echo "Enter MySQL root passowrd:"
-
-cat << EOS | mysql -u root -p
-CREATE USER $MYSQL_USER;
-CREATE DATABASE $DATABASE_NAME;
-GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO "$MYSQL_USER"@"localhost" IDENTIFIED BY "$MYSQL_PASSWORD";
-FLUSH PRIVILEGES;
-EOS
-
 echo "wget -P /tmp/ http://ja.wordpress.org/latest-ja.tar.gz"
 wget -P /tmp/ http://ja.wordpress.org/latest-ja.tar.gz
 echo "tar zxvf /tmp/latest-ja.tar.gz -C /tmp/"
