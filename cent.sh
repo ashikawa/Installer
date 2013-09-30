38cbed49cb80567d2c594439050de68bd5056e81
s.ashikawa <a.shigeru@gmail.com>
2013-09-30 12:27:10 +0900

    Update cent.sh

--- cent.sh
+++ cent.sh
@@ -9,6 +9,9 @@ yum install -y gcc gcc-c++ make openssl-devel git subversion telnet sysstat
 echo "yum --enablerepo=epel -y install inotify-tools"
 yum --enablerepo=epel -y install inotify-tools
 
+echo "yum -y install zsh"
+yum -y install zsh
+
 # SE Linux のオフ
 setenforce 0
 sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
