2e5e130dd76236b19171cc7b33699768c332c173
a.shigeru <a.shigeru@gmail.com>
2013-11-29 11:44:44 +0900

    apc 設定を追加

--- php.sh
+++ php.sh
@@ -6,6 +6,7 @@ yum install -y php php-devel php-cli php-mbstring php-pear php-xml php-gd php-my
 sed -i -e 's#memory_limit = .*M#memory_limit = 512M#g' /etc/php.ini
 sed -i -e 's#;date.timezone =#date.timezone = "Asia/Tokyo"#g' /etc/php.ini
 sed -i -e 's#expose_php =.*#expose_php = Off#g' /etc/php.ini
+sed -i -e 's/apc.shm_size=64M/apc.shm_size=256M/g' /etc/php.d/apc.ini
 
 echo "/etc/init.d/httpd restart"
 /etc/init.d/httpd restart
